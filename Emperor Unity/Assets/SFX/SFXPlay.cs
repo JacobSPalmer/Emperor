cc3b67e6f2423f12f6424645f204f9fd35d1d7a6
JacobSPalmer <58527341+JacobSPalmer@users.noreply.github.com>
2021-04-23 14:40:20 -0500

    add sfx to menu buttons and game start

---
+++ Emperor Unity/Assets/SFX/SFXPlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlay : MonoBehaviour
+{
+    public AudioSource ButtonClick;
+    public AudioSource CardClick;
+
+    public AudioSource StartingGame;
+
+    public void PlayButtonClick() {
+        ButtonClick.Play();
+    }
+
+    public void PlayCardClick(){
+        CardClick.Play();
+    }
+
+    public void PlayStartingGameClick() {
+        StartingGame.Play();
+    }
+}
--- Emperor Unity/Assets/Scripts/UserInput.cs
+++ Emperor Unity/Assets/Scripts/UserInput.cs
@@ -239,4 +239,6 @@ public class UserInput : MonoBehaviour
         return true;
 
     }
+
+    // bool CheckWinState(){}
 }
